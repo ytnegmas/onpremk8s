f9d60377e8dbd21e2ee4262b4f23095e627bd83f
sgenty <s.genty@striveworks.com>
2025-08-19 11:54:15 -0700

    Add observability instrumentation + build tests

--- apps/simple-webapp/app.py
+++ apps/simple-webapp/app.py
@@ -1,11 +1,36 @@
-from datetime import datetime, timezone
 import os
+import logging, sys, json
+from datetime import datetime, timezone
 from flask import Flask, jsonify
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 
 APP_NAME = os.getenv("APP_NAME", "simple-webapp")
 BUILD_SHA = os.getenv("BUILD_SHA", "unknown")
+
+# Observability instrumentation
+class JsonFormatter(logging.Formatter):
+    def format(self, record):
+        log_record = {
+            "time": self.formatTime(record, self.datefmt),
+            "level": record.levelname,
+            "message": record.getMessage(),
+            "logger": record.name,
+        }
+        return json.dumps(log_record)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(JsonFormatter())
+
+# opens up some default metrics out of the box with prometheus
+# request count, latency, etc.
+metrics = PrometheusMetrics(app)
+metrics.info("simple_webapp", "Simple Webapp info", version=BUILD_SHA)
+
+app.logger.addHandler(handler)
+app.logger.setLevel(logging.INFO)
+###
 
 @app.get("/info")
 def info():
